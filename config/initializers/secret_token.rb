07559febdd69cfdc99f0467f98d0c4a83e176b9c
Ivan Stroganov <knowhere@gmail.com>
2012-01-07 16:14:11 -0800

    first commit, rails project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperboy::Application.config.secret_token = 'd87643635d5380727e4432c5f4387a418ba91d720d4ee4c212ef4820a67870d95bddd12b1690176a0b543ac896eaf94f017e3b4bc8aff28950fef506c54d3400'
